b9bdf773262fbeb87cb42a1912a28546b2a62c0d
Cillebokin <swcillebokin@gmail.com>
2026-05-12 13:38:11 +0800

    release: 更新版本号与版本说明

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.comiclab"
         minSdk = 33
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1.0512"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
